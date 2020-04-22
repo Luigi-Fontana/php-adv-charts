0f623fda35728d142cf95c41dd886456d04da98e
Luigi Fontana <luigifontana92@gmail.com>
2020-04-22 15:39:36 +0200

    Creazione dei File e installazione Laravel Mix

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
+        <link rel="shortcut icon" href="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimage.flaticon.com%2Ficons%2Fpng%2F512%2F138%2F138339.png&f=1&nofb=1">
+        <title>ADS | Charts</title>
+        <link rel="stylesheet" href="dist/css/app.css">
+    </head>
+    <body>
+
+
+        <script src="dist/js/app.js" charset="utf-8"></script>
+    </body>
+</html>
